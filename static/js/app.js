6634fb68f61df33b8fd580cc14339db8eab9f058
Janelle Anderson <janelleanderson@Janelles-MacBook-Air-4.local>
2021-07-03 13:08:33 -0400

    site customized

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,7 @@ function buildTable (data) {
         // create variable to append a row to the HTML table body 
         let row = tbody.append("tr");
 
+
         //loop through each fiels in the dataRow argument
         Object.values(dataRow).forEach((val) => {
             // append each value of the object to a cell in the table
@@ -31,16 +32,16 @@ function handleClick() {
     //select elements that matches #datetime
     //chain .propety("value"); to search for date values
     //grab the information & hold in the date variable 
-    let date = d3.select("#datatime").property("value");
+    let date = d3.select("#datetime").property("value");
     //set default filter & save to a new variable
     let filteredData = tableData;
     //use if statement to create date filter
     if (date) {
         filteredData = filteredData.filter(row => row.datetime === date);
-    }
+    };
     //rebuild the table using filtered data
     buildTable(filteredData);
-}
+};
 
 //attach an event to listen for the form button 
 d3.selectAll("#filter-btn").on("click", handleClick);
